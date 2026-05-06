8b0f570c472963bf1cae1a8d0a1ad972b8578fc5
lhossohmida-lang <lhossohmida@gmail.com>
2026-05-06 18:37:30 +0200

    Allow Firebase app origin for local AI backend

--- server/server.js
+++ server/server.js
@@ -29,6 +29,8 @@ const allowedOrigins = new Set([
   'http://127.0.0.1:3000',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://dajaj-ydbah.web.app',
+  'https://dajaj-ydbah.firebaseapp.com',
 ]);
 
 app.use(
